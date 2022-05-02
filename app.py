2400387bb5d4fbfaa778e5654efc182b9112f4ca
soootaleb <talebs@icloud.com>
2022-05-02 16:32:56 +0200

    add API

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-import sklearn
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World from Python'
+
+
+if __name__ == "__main__":
+  app.run()
